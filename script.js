7739524c05c494fdc05581516c26720391ff461c
JoGoRef <johannes.refsnes@gmail.com>
2025-06-04 20:20:42 +0200

    Santorini nettside, begynt

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 
-document.getElementById("lesMerBtn").addEventListener("click", function () {
+  document.getElementById("lesMerBtn").addEventListener("click", function () {
     document.getElementById("land").scrollIntoView({ behavior: "smooth" });
-});
+  });
+
+  document.getElementById("seTurerBtn").addEventListener("click", function () {
+    document.getElementById("land").scrollIntoView({ behavior: "smooth" });
+  });
