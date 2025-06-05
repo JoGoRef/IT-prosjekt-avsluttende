45a42b998f46c258875e887785a246ac970e4b38
JoGoRef <johannes.refsnes@gmail.com>
2025-06-05 09:17:41 +0200

    fiksa NAV

--- script.js
+++ script.js
@@ -6,3 +6,7 @@
   document.getElementById("seTurerBtn").addEventListener("click", function () {
     document.getElementById("land").scrollIntoView({ behavior: "smooth" });
   });
+
+  document.getElementById("tilbud").addEventListener("click", function () {
+    document.getElementById("rabatt").scrollIntoView({ behavior: "smooth" });
+  });
