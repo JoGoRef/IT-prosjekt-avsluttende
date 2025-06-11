9bb93ff4e7dc2d6c594e417e9b9fcad2a93e75d3
JoGoRef <johannes.refsnes@gmail.com>
2025-06-11 14:03:26 +0200

    javascript kontakter

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 
-  document.getElementById("lesMerBtn").addEventListener("click", function () {
-    document.getElementById("land").scrollIntoView({ behavior: "smooth" });
-  });
+document.getElementById("lesMerBtn").addEventListener("click", function () {
+  document.getElementById("land").scrollIntoView({ behavior: "smooth" });
+});
 
-  document.getElementById("seTurerBtn").addEventListener("click", function () {
-    document.getElementById("land").scrollIntoView({ behavior: "smooth" });
-  });
+document.getElementById("seTurerBtn").addEventListener("click", function () {
+  document.getElementById("land").scrollIntoView({ behavior: "smooth" });
+});
 
-  document.getElementById("tilbud").addEventListener("click", function () {
-    document.getElementById("rabatt").scrollIntoView({ behavior: "smooth" });
-  });
+document.getElementById("tilbud").addEventListener("click", function () {
+  document.getElementById("rabatt").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById('kontaktSkjema').addEventListener('submit', function (e) {
+  e.preventDefault();
+  document.getElementById('successMessage').style.display = 'block';
+  this.reset();
+});
